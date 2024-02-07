1ae5d460d3af27e9b3022aba63483e388b6a4fa5
NaoyaMatsunaga <>
2024-02-07 23:39:34 +0900

    widgetを組み立ててマイページの上半分を作成

--- lib/pages/my_page.dart
+++ lib/pages/my_page.dart
@@ -9,6 +9,108 @@ class MyPage extends StatelessWidget {
       appBar: AppBar(
         title: const Text("マイページ"),
       ),
+      body: Padding(
+        padding: const EdgeInsets.all(16),
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            Row(
+              children: [
+                Image.network(
+                  "https://cdn.icon-icons.com/icons2/2699/PNG/512/instagram_logo_icon_168715.png",
+                  width: 60,
+                  height: 60,
+                ),
+                const Spacer(),
+                const Column(children: [
+                  Text("7000",
+                      style:
+                          TextStyle(fontWeight: FontWeight.bold, fontSize: 16)),
+                  Text("投稿")
+                ]),
+                const SizedBox(width: 16),
+                const Column(children: [
+                  Text("4.6億",
+                      style:
+                          TextStyle(fontWeight: FontWeight.bold, fontSize: 16)),
+                  Text("フォロワー")
+                ]),
+                const SizedBox(width: 8),
+                const Column(children: [
+                  Text("99",
+                      style:
+                          TextStyle(fontWeight: FontWeight.bold, fontSize: 16)),
+                  Text("フォロー中")
+                ]),
+              ],
+            ),
+            const Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                SizedBox(height: 16),
+                Text("Instagram",
+                    style:
+                        TextStyle(fontWeight: FontWeight.bold, fontSize: 16)),
+                Text("Instagram", style: TextStyle(color: Colors.blue)),
+                Text("Instagram", style: TextStyle(color: Colors.blue)),
+              ],
+            ),
+            const SizedBox(
+              height: 16,
+            ),
+            Row(
+              mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+              children: [
+                Expanded(
+                  child: Row(
+                    children: [
+                      Expanded(
+                        child: OutlinedButton(
+                          onPressed: () {},
+                          style: OutlinedButton.styleFrom(
+                              shape: RoundedRectangleBorder(
+                                  borderRadius: BorderRadius.circular(4))),
+                          child: const Text(
+                            "フォロー中",
+                            style: TextStyle(
+                              fontWeight: FontWeight.bold,
+                              fontSize: 12,
+                              color: Colors.black,
+                            ),
+                          ),
+                        ),
+                      ),
+                      const SizedBox(width: 4),
+                      Expanded(
+                        child: OutlinedButton(
+                          onPressed: () {},
+                          style: OutlinedButton.styleFrom(
+                              shape: RoundedRectangleBorder(
+                                  borderRadius: BorderRadius.circular(4))),
+                          child: const Text(
+                            "メッセージ",
+                            style: TextStyle(
+                                fontWeight: FontWeight.bold,
+                                fontSize: 12,
+                                color: Colors.black),
+                          ),
+                        ),
+                      ),
+                    ],
+                  ),
+                ),
+                const SizedBox(width: 4),
+                OutlinedButton(
+                    onPressed: () {},
+                    style: OutlinedButton.styleFrom(
+                        shape: RoundedRectangleBorder(
+                            borderRadius: BorderRadius.circular(4))),
+                    child: const Icon(Icons.keyboard_arrow_down)),
+              ],
+            ),
+          ],
+        ),
+      ),
     );
   }
 }
